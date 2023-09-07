86914bc8566432c2ad5b6759cdee255f984173b0
JuliusPja <138770275+JuliusPja@users.noreply.github.com>
2023-09-07 16:25:44 +0100

    added navbar

---
+++ portfolio/src/Pages/Home.js
@@ -0,0 +1,16 @@
+import Navbar from "../components/Navbar";
+
+export default function Home(){
+
+  // const [currentView, setCurrentView] = useState('view1'); // Initial view
+
+  // const changeView = (view) => {
+  //   setCurrentView(view);
+  // };
+
+  return(
+    <div>
+      <Navbar/>
+    </div>
+  )
+}
---
+++ portfolio/src/components/Navbar.jsx
@@ -0,0 +1,54 @@
+import "../App.css"
+import "./Navbar.css"
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+export default function Navbar(){
+
+  // MENU ICON DROPDOWN HOOK
+
+  const [menu, setMenu] = useState({clicked: false});
+  const handleMenuClick = () => {
+    setMenu({clicked: !menu.clicked})
+  };
+
+  return(
+    <>
+      
+       {/* DESKTOP NAVBAR */}
+
+      <div id="desktop">
+        <img className="Logo" src="./assets/Logo.png" alt="Logo" />
+              <nav className={menu.clicked ? 
+              "DesktopNavbar active" : "DesktopNavbar"}>
+                  <ul 
+                  id="navbar"
+                  className={menu.clicked ? 
+                  "#navbar active" : "#navbar"}>
+                    <li>
+                      <Link to="/" className="FontNavbar active">Home</Link>
+                    </li>
+                    <li>
+                      <Link to="/about" className="FontNavbar">About</Link>
+                    </li>
+                    <li>
+                      <Link to="/projects" className="FontNavbar">Projects</Link>
+                    </li>
+                    <li>
+                      <Link to="/contact" className="FontNavbar">Contact</Link>
+                    </li>
+                  </ul>
+                  {menu.clicked ? 
+              <span onClick={handleMenuClick} 
+              className="material-symbols-outlined" id="icon_close">close</span> 
+              : 
+              <span onClick={handleMenuClick} className="material-symbols-outlined" 
+                id="icon_menu">menu</span>
+                }
+              </nav> 
+      </div>
+
+
+    </>
+  )
+}
