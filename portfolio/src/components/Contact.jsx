3d52e6a0378b62c523a77a965e3fcd93d02ab6bc
JuliusPja <138770275+JuliusPja@users.noreply.github.com>
2023-09-09 16:58:47 +0100

    added conditional rules to the contact form

--- portfolio/src/components/Contact.jsx
+++ portfolio/src/components/Contact.jsx
@@ -12,6 +12,24 @@ export default function Contact(){
   const sendEmail = (e) => {
     e.preventDefault();
 
+  // Access form elements by name and retrieve their values
+  const userName = form.current.user_name.value;
+  const userEmail = form.current.user_email.value;
+  const message = form.current.message.value;
+
+
+  // Conditional rules
+  
+    if (userName === ''){
+      alert('missing name')
+    }
+    else if (userEmail === ''){
+      alert('missing email')
+    }
+    else if (message === ''){
+      alert('missing message')
+    }
+    else{
     emailjs.sendForm(
       'service_bd41em7', 
       'template_lgbeiam', 
@@ -25,16 +43,18 @@ export default function Contact(){
       }, (error) => {
           alert(error.text);
       });
+    };
   };
   
   return(
     <>
+    { }
       <h2 className="FontH2">contact</h2>
       <div className="ContactContainer">
       <form ref={form} onSubmit={sendEmail} className="FormContainer">     
-      <input type="text" name="user_name" placeholder="Name" className="FontGeneral Box"/>
-      <input type="email" name="user_email" placeholder="Email" className="FontGeneral Box"/>
-      <textarea name="message" placeholder="Message" className="FontGeneral Box L"/>
+      <input id="user_name" type="text" name="user_name" placeholder="Name" className="FontGeneral Box"/>
+      <input id="user_email" type="email" name="user_email" placeholder="Email" className="FontGeneral Box"/>
+      <textarea id="message" name="message" placeholder="Message" className="FontGeneral Box L"/>
       <input type="submit" value="Send" className="FontButton Button"/>
       </form>
       </div>
